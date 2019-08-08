358f135922aeeff4760a7c192c36e82d4961bec5
AnanyaMaruthi <46527922+AnanyaMaruthi@users.noreply.github.com>
2019-08-08 15:11:42 +0530

    Add files via upload

--- 5th semester/ADALab/binarySearch.cpp
+++ 5th semester/ADALab/binarySearch.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int* bubbleSort(int array[], int size){
+	int sorted[size];
+	int temp;
+	for(int i = 0; i < size; i++){
+		for(int j = i; j < size; j++){
+			if(array[j] < array[i]){
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	return array;
+}
+
 int binarySearch(int element, int array[], int size){
 	int low = 0;
 	int high = size - 1;
@@ -22,20 +37,22 @@ int binarySearch(int element, int array[], int size){
 
 int main(){
 	int array[10];
-//	cout << "Enter elements: " << endl;
+	cout << "Enter elements: " << endl;
 	for(int i = 0; i < 10; i++){
 		array[i] = rand() % 10000;
 	}
+	int* sortedArray;
+	sortedArray = bubbleSort(array, 10);
 	cout << "Array elements are: " << endl;
 	for(int i = 0; i < 10; i++){
-		cout << array[i] << " ";
+		cout << sortedArray[i] << " ";
 	}
 	int element;
 	cout << endl;
 	cout << "Enter element";
 	cin >> element;
 	int position;
-	position = binarySearch(element, array, 10);
+	position = binarySearch(element, sortedArray, 10);
 	if(position < 0){
 		cout << "Element not found";
 	}
