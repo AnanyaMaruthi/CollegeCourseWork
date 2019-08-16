cb2df02f31538b6e0264e1783cbc28d282cbfce9
AnanyaMaruthi <46527922+AnanyaMaruthi@users.noreply.github.com>
2019-08-16 09:43:28 +0530

    Update Program-1b.py

--- 5th semester/PythonLab/1BM17CS015/Program-1b.py
+++ 5th semester/PythonLab/1BM17CS015/Program-1b.py
@@ -1,12 +1,14 @@
 #Fibonacci nummbers
 
 def fibonacci(number):
-    series = [0 for _ in range(number + 2)]
+    series = [0 for _ in range(number)]
     series[1] = 1
     for i in range(2, number + 1):
         series[i] = series[i - 1] + series[i - 2]
-    return series[number]
+    return series
 
 number = int(input("Enter the number: "))
-fibonacciNumber = fibonacci(number)
-print("The fibonacci number is", fibonacciNumber)
+series = fibonacci(number)
+print("The fibonacci series is")
+for x in series:
+    print(x, end = " ")
