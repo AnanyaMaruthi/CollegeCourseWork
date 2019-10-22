a15dc9b6a4286ab7b769f0888a505db922f35184
Ananya <ananyasheshu@gmail.com>
2019-10-22 20:58:27 +0530

    Refactored code

--- 5th semester/IoTLab/LDR.ino
+++ 5th semester/IoTLab/LDR.ino
@@ -1,28 +1,26 @@
 int LED = 11;
-int sensor = A0;
-void setup(){
-  pinMode(LED, OUTPUT);
-  pinMode(sensor, INPUT);
-  Serial.begin(9600);
+int lightSensor = A0;
+void setup()
+{
+    pinMode(LED, OUTPUT);
+    // pinMode(lightSensor, INPUT);
+    Serial.begin(9600);
 }
 
-void loop(){
-  int sensedValue = analogRead(sensor);
-  Serial.println(sensedValue);
-  delay(100);
-
-//  if(sensedValue < 200){
-//  
-//    digitalWrite(LED, HIGH);
-//    Serial.println(sensedValue);
-//    delay(100);
-//    
-//  }
-//  else
-//  {
-//    digitalWrite(LED, LOW);
-//    Serial.println(sensedValue);
-//    delay(100);
-//  }
-  
+void loop()
+{
+    int sensedValue = analogRead(lightSensor);
+    Serial.println(sensedValue);
+    if (sensedValue < 200)
+    {
+        digitalWrite(LED, HIGH);
+        Serial.println(sensedValue);
+        delay(100);
+    }
+    else
+    {
+        digitalWrite(LED, LOW);
+        Serial.println(sensedValue);
+        delay(100);
+    }
 }
