79fc07ec2bed889ee36b578b59fe2947a79691a4
Ananya <ananyasheshu@gmail.com>
2019-10-22 21:12:11 +0530

    Refactored code

--- 5th semester/IoTLab/Temperature_sensor.ino
+++ 5th semester/IoTLab/Temperature_sensor.ino
@@ -1,15 +1,17 @@
-int input = 2;
-void setup(){
-  Serial.begin(9600);
+int sensor = 2;
+void setup()
+{
+	Serial.begin(9600);
 }
 
-void loop(){
-  int rawVoltage = analogRead(input);
-  float millivolts = (rawVoltage / 1024.0) * 5000;
-  float celsius = millivolts / 10;
-  Serial.print(celsius);
-  Serial.print(" degree celsius \n");
-  Serial.print((celsius * 9) / 5 + 32);
-  Serial.print(" degree farenheit\n");
-  delay(5000);
+void loop()
+{
+	int rawVoltage = analogRead(sensor);
+	float millivolts = (rawVoltage / 1024.0) * 5000;
+	float celsius = millivolts / 10;
+	Serial.print(celsius);
+	Serial.print(" degree celsius \n");
+	Serial.print((celsius * 9) / 5 + 32);
+	Serial.print(" degree farenheit\n");
+	delay(5000);
 }
