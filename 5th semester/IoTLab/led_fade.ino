1bb9b8d6d9ee58e76e9b2011d61423df3ecb1ef9
Ananya <ananyasheshu@gmail.com>
2019-10-22 21:07:54 +0530

    Refactored code

--- 5th semester/IoTLab/led_fade.ino
+++ 5th semester/IoTLab/led_fade.ino
@@ -1,18 +1,20 @@
-//int LED = 11;
+int LED = 11;
 
-void setup(){
-  pinMode(LED, OUTPUT);
+void setup()
+{
+    pinMode(LED, OUTPUT);
 }
 
-void loop(){
-
-  int i;
-  for(i = 0; i <= 255; i += 5){
-    analogWrite(LED, i);
-    delay(100);
-  }
-  for(i = 255; i >= 0; i -= 5){
-    analogWrite(LED, i);
-    delay(100);
-  }
+void loop()
+{
+    for (int i = 0; i <= 255; i += 5)
+    {
+        analogWrite(LED, i);
+        delay(100);
+    }
+    for (int i = 255; i >= 0; i -= 5)
+    {
+        analogWrite(LED, i);
+        delay(100);
+    }
 }
