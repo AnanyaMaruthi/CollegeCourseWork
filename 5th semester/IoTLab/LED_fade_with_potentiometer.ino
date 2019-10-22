60fe75161e97d2f3dc372f8c409de498d4ed787d
Ananya <ananyasheshu@gmail.com>
2019-10-22 21:02:32 +0530

    Refactored code

--- 5th semester/IoTLab/LED_fade_with_potentiometer.ino
+++ 5th semester/IoTLab/LED_fade_with_potentiometer.ino
@@ -3,20 +3,20 @@ int potentiometer = 2;
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  pinMode(potentiometer, INPUT); //Not required
-  Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    pinMode(potentiometer, INPUT); //Not required
+    Serial.begin(9600);
 }
 
 void loop()
 {
-  int sensedValue = analogRead(potentiometer);
-  //  sensedValue = (sensedValue / 1024) * 256;
-  int brightness = map(sensedValue, 0, 1023, 0, 255);
-  Serial.print("Input");
-  Serial.print(sensedValue);
-  Serial.print("Converted value");
-  Serial.println(brightness);
-  delay(2000);
-  analogWrite(LED, brightness);
+    int sensedValue = analogRead(potentiometer);
+    //  sensedValue = (sensedValue / 1024) * 256;
+    int brightness = map(sensedValue, 0, 1023, 0, 255);
+    Serial.print("Input");
+    Serial.print(sensedValue);
+    Serial.print("Converted value");
+    Serial.println(brightness);
+    delay(2000);
+    analogWrite(LED, brightness);
 }
