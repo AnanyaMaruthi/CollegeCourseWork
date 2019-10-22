6b4e6236ca99b39843fb8f9f6f32c8d61145cd9b
Ananya <ananyasheshu@gmail.com>
2019-10-22 20:58:56 +0530

    Refactored code

--- 5th semester/IoTLab/LED_blink.ino
+++ 5th semester/IoTLab/LED_blink.ino
@@ -1,12 +1,14 @@
 int LED = 13;
 
-void setup(){
-  pinMode(LED, OUTPUT);
+void setup()
+{
+    pinMode(LED, OUTPUT);
 }
 
-void loop(){
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(500);
+void loop()
+{
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(500);
 }
