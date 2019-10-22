8e189ca321e371156355a8943a5f2acefcd856e5
Ananya <ananyasheshu@gmail.com>
2019-10-22 20:56:15 +0530

    Renamed file  and refactored code

--- 5th semester/IoTLab/RGB_LED.ino
+++ 5th semester/IoTLab/RGB_LED.ino
@@ -2,22 +2,22 @@ int red = 2;
 int green = 3;
 int blue = 4;
 
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(red, HIGH);
-  delay(3000);
-  for(int i = 0; i <=255; i = i + 25){
-    for(int j = 0; j <= 255; j = j + 25){
-      for(int k = 0; k <= 255; k = k + 25){
+void loop()
+{
+  for (int i = 0; i <= 255; i = i + 25)
+  {
+    for (int j = 0; j <= 255; j = j + 25)
+    {
+      for (int k = 0; k <= 255; k = k + 25)
+      {
         analogWrite(red, i);
         analogWrite(green, j);
         analogWrite(blue, k);
@@ -26,9 +26,8 @@ void loop() {
         Serial.print(j);
         Serial.print(" ");
         Serial.println(k);
-        delay(100);
+        delay(10);
       }
     }
   }
-
 }
