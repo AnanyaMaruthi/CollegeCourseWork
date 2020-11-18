325f64abb871504767dc2349504dc8c750c22ec7
AnanyaMaruthi <ananyasheshu@gmail.com>
2020-11-18 15:24:09 +0530

    Lab 8

--- 7th semester/DataScienceWithR/Lab8/Question1.R
+++ 7th semester/DataScienceWithR/Lab8/Question1.R
@@ -1,8 +1,11 @@
 library(visualize)
 
+# null hypothesis -> means are equal
+# alternative hypothesis -> means are not equal
+
 machine1 <- c(363,404,518,521,613,587,412,469,468, 496)
 machine2 <- c(536,474,556,549,479,422,414,505,505,552)
-alpha <- 0.95
+alpha <- 0.05
 machine1_mean <- mean(machine1)
 machine1_mean
 machine2_mean <- mean(machine2)
@@ -11,11 +14,9 @@ var(machine1)
 sd(machine1)
 var(machine2)
 sd(machine2)
-t.test(x = machine1, y = machine2, var.equal = TRUE, conf.level = alpha)
+t.test(x = machine1, y = machine2, var.equal = TRUE, conf.level = 1 - alpha)
+
+# p-value = 0.6436 > alpha (=0.05) hence, reject null hypothesis and accept alternative hypothesis
+# degrees of freedom = df = n1 + n2 - 2 = 10 + 10 - 2 = 18
 
 visualize.t(stat=c(-0.47061, 0.47061), df=18, section="tails")
-visualize.t(stat=c(-0.47061), df=18, section="lower")
-visualize.t(stat=c(0.47061), df=18, section="upper")
-
-qt(p = 0.05/2, df = 18, lower.tail = FALSE)
-qt(p = 0.05/2, df = 18, lower.tail = TRUE)
